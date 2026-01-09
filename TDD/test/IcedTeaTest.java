864d97888f7f6780e24af28a00f484626dae1b36
RKNash <145068529+RyanKNash@users.noreply.github.com>
2026-01-09 12:07:23 -0500

    ...

---
+++ TDD/test/IcedTeaTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class IcedTeaTest {
+
+    @Test
+    public void iced_tes_created_with_50mg_sugar_by_default() {
+        IcedTea icedTea = new IcedTea();
+    }
+
+}
